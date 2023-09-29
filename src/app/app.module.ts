49e58b4ec1a6bf1459dc873f2b4857f1b4f0ebee
JoniMeichtry11 <joni.meichtry@gmail.com>
2023-09-28 23:19:05 -0300

    feat: global components and init first code from web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { LoaderComponent } from './core/components/loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
